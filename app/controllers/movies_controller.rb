bd7c1fb63fbf3ecf0f89ec7807b8676b4d9b2de1
Jaroslaw Zmijewski <jzmija@wp.pl>
2018-12-08 19:42:26 +0100

    Create action for Show, Edit, Update, Destroy, Find_movie

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,7 +1,7 @@
 class MoviesController < ApplicationController
- 
+    before_action :find_movie, only: [:show, :edit, :update, :destroy]
   def index
-
+    @movies = Movie.all.order(:name)
   end
 
   def new
@@ -18,25 +18,40 @@ class MoviesController < ApplicationController
   end
 
   def show
-
+    # @movie = Movie.find(params[:id])
+    # find_movie
   end
 
   def edit
-
+    # @movie = Movie.find(params[:id])
+    # find_movie
   end
 
   def update
-
+   # @movie = Movie.find(params[:id])
+   # find_movie
+      if @movie.update(movie_params)
+        redirect_to movie_path(@movie)
+      else
+        render 'edit'
+      end
   end
 
   def destroy
-
+   # @movie = Movie.find(params[:id])
+   # find_movie
+    @movie.destroy
+    redirect_to movies_path
   end
 
 private
 
   def movie_params
     params.require(:movie).permit(:name, :premiere_date,:length, :description, :genre, :director, :scenarist, :country)
+  end
+
+  def find_movie
+    @movie = Movie.find(params[:id])
   end
 
 end
