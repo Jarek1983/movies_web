eef6ced3600439669f7f7d8dfabfe94b54dbbfd5
Jaroslaw Zmijewski <jzmija@wp.pl>
2018-12-08 21:07:22 +0100

    Add validates for models/movie

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,12 @@
 class Movie < ApplicationRecord
+
+  validates :name, presence: true, length: {minimum: 2}
+  validates :premiere_date, presence: true
+  validates :length, presence: true, length: {minimum: 1}
+  validates :description, presence: true, length: {minimum: 1}
+  validates :genre, length: {minimum: 5}
+  validates :director, presence: true
+  validates :scenarist, presence: true
+  validates :country, presence: true, length: {minimum: 3}
+  
 end
