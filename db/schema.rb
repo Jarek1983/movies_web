e2445825ea6d9532d40ecbdf2c7f111c17b118da
Jaroslaw Zmijewski <jzmija@wp.pl>
2018-12-17 18:06:04 +0100

    Create Users controller and model, doing migrate, create action index, new, create, user_params for this controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_08_204518) do
+ActiveRecord::Schema.define(version: 2018_12_17_162753) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,14 @@ ActiveRecord::Schema.define(version: 2018_12_08_204518) do
     t.string "director"
     t.string "scenarist"
     t.string "country"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "userid"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
