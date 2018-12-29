301c74f16a3d1b7254b55a7a23240a6e72789575
Jaroslaw Zmijewski <jzmija@wp.pl>
2018-12-29 23:07:03 +0100

    Correct model/movie

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -4,7 +4,6 @@ class Movie < ApplicationRecord
   validates :premiere_date, presence: true
   validates :length, presence: true, length: {minimum: 1}
   validates :description, presence: true, length: {minimum: 1}
-  validates :scenarist, presence: true
   validates :country, presence: true
   
     has_many :comments, dependent: :destroy
